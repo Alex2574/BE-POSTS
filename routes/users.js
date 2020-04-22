c84635e18e916eb7f7c1458034deb2f1739c367a
Alex2574 <aleksandr.strela@yahoo.com>
2020-04-22 15:36:58 +0300

    Add posts mock on GET

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var getPostsData = require('./get-posts.json');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/posts.json', function(req, res, next) {
+  // res.send(getPostsData);
+  res.status(200).json(getPostsData);
 });
 
 module.exports = router;
