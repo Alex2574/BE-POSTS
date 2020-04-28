98fd5ed553bd77e5af899b326f5c9340249bd451
Alex2574 <aleksandr.strela@yahoo.com>
2020-04-28 15:17:38 +0300

    Added post with comments inside every post

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var users = require('./routes/users');
 var app = express();
+var cors = require('cors');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var getPostsData = require('./get-posts.json');
+var getPostData = require('./posts/-M3FmQy1BjEM9069EZv_.json');
+var getPostData = require('./posts/-M3Fmj4xCQscuk-mSY5-.json');
+var getPostData = require('./posts/-M3Fn-i1lbRpP-V7UsUs.json');
+var getPostData = require('./posts/-M4sXN9CmF-ZI-4LGlrP.json');
 
 /* GET users listing. */
 router.get('/posts.json', function(req, res, next) {
   // res.send(getPostsData);
   res.status(200).json(getPostsData);
+});
+
+router.get('/posts/-M3FmQy1BjEM9069EZv_.json', function(req, res, next) {
+  res.status(200).json(getPostData);
+});
+
+router.get('/posts/-M3Fmj4xCQscuk-mSY5-.json', function(req, res, next) {
+  res.status(200).json(getPostData);
+});
+router.get('/posts/-M3Fn-i1lbRpP-V7UsUs.json', function(req, res, next) {
+  res.status(200).json(getPostData);
+});
+router.get('/posts/-M4sXN9CmF-ZI-4LGlrP.json', function(req, res, next) {
+  res.status(200).json(getPostData);
 });
 
 module.exports = router;
